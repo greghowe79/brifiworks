b2f22b4b85db81d946956c700895ecd221409043
fily1212 <fily1212@gmail.com>
2020-10-26 16:43:23 +0100

    navbar

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import logo from '../../assets/logo.png';
 import { Header } from '../../Styled/Header';
 import { Container } from '../../Styled/Container';
